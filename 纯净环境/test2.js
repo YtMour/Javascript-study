8ac56d1ac995137c16dc21614934f16f8c9bae0b
YtMour <ResidualStar61@gmail.com>
2024-10-26 14:26:03 +0800

    Update test2.js

--- 纯净环境/test2.js
+++ 纯净环境/test2.js
@@ -214,26 +214,202 @@
 
 
 //简写
-class Person{
-  //似有属性   #开头    必须先声明不可省略
-  #name;
-  #age =18;
-  constructor(name,age){
-    this.#name=name;
-    this.#age=age;
-  }
-  get age(){return this.#age;}
-  set age(age){
-    if (age>=0 && age<=120){
-    }else{
-      console.log('年龄不合法');
-      return;
-    }
-    this.#age=age;
-  }
+// class Person{
+//   //似有属性   #开头    必须先声明不可省略
+//   #name;
+//   #age =18;
+//   constructor(name,age){
+//     this.#name=name;
+//     this.#age=age;
+//   }
+//   get age(){return this.#age;}
+//   set age(age){
+//     if (age>=0 && age<=120){
+//     }else{
+//       console.log('年龄不合法');
+//       return;
+//     }
+//     this.#age=age;
+//   }
 
+// }
+//   let Person1=new Person('Tom',20);
+//   Person1.age=25;
+
+//   console.log(Person1.age);
+
+
+
+
+//继承
+// class Person{
+//   //私有属性   #开头    必须先声明不可省略
+//   #name;
+//   #age;
+//   #sex;
+//   constructor(name,age,sex){
+//     this.#name=name;
+//     this.#age=age;
+//     this.#sex=sex;
+//   }
+//   get name(){return this.#name;}
+//   get age(){return this.#age;}
+//   get sex(){return this.#sex;}
+
+// }
+// let p1=new Person('Tom',20,'male');
+// console.log(p1.name,p1.age,p1.sex);
+
+// class Teacher extends Person{
+//   teach(){
+//     console.log('教学');
+//   }
+// }
+// let p2 =new Teacher('Jerry',30,'male');
+// console.log(p2.name);
+// p2.teach();
+
+
+
+// class Student extends Person{
+//   study(){
+//     console.log('学习');
+//   }
+// }
+
+// let p3=new Student('Lily',25,'female');
+// console.log(p3.name);
+// p3.study();
+
+
+
+
+// class Person{
+//   //似有属性   #开头    必须先声明不可省略
+//   #name;
+//   #age;
+//   #sex;
+//   constructor(name,age,sex){
+//     this.#name=name;
+//     this.#age=age;
+//     this.#sex=sex;
+//   }
+//   get name(){return this.#name;}
+//   get age(){return this.#age;}
+//   get sex(){return this.#sex;}
+//   sayHello(){
+//     console.log(`Hello,${this.#name}`);
+//   }
+// }
+// let p1=new Person('Tom',20,'male');
+// console.log(p1.name,p1.age,p1.sex);
+// p1.sayHello();
+
+// class Teacher extends Person{
+//   teach(){
+//     console.log('教学');
+//   }
+//   sayHello(){
+//     console.log(`Hello,${this.name}`);
+//   }
+//   toString(){
+//     return `${this.name} is a teacher`;
+//   }
+// }
+
+// let p2 =new Teacher('Jerry',30,'male');
+// console.log(p2.name);
+// p2.teach();
+// p2.sayHello();
+// console.log(p2,p2.name);
+// console.log(p2.toString());
+
+
+
+
+
+//重写构造函数
+// class Person{
+
+//   #name;
+//   #age;
+//   #sex;
+//   constructor(name,age,sex){
+//     this.#name=name;
+//     this.#age=age;
+//     this.#sex=sex;
+//   }
+//   get name(){return this.#name;}
+//   set name(name){this.#name=name;}
+//   get age(){return this.#age;}
+//   get sex(){return this.#sex;}
+//   sayHello(){
+//     console.log(`Hello,Person`);
+//   }
+//   static test(){
+//     console.log('test Person');
+//   }
+// }
+
+
+// class Teacher extends Person{
+//   constructor(name,age,sex){
+//       super(name,age,sex);
+//       this.name=name+' teacher';
+//       this.flag=1;
+//     }
+//   teach(){
+//     console.log('教学');
+//     //this.sayHello();
+//     super.sayHello();
+//   }
+//   sayHello(){
+//     console.log(`Hello,Teacher`);
+//   }
+//   static test(){
+//     console.log('test Teacher');
+//   }
+
+// }
+
+// let p2 =new Teacher('Jerry',30,1);
+// console.log(p2.name);
+// console.log(p2);
+// p2.teach();
+// Person.test();
+// Teacher.test();
+
+
+
+//多态
+class Demo{
+  sayHello(){
+    console.log('Hello,Demo');
+  }
 }
-  let Person1=new Person('Tom',20);
-  Person1.age=25;
 
-  console.log(Person1.age);
+class Test extends Demo{
+  sayHello(){
+    console.log('Hello,Test');
+  }
+}
+//要自己额外进行检查   校验
+function abc(obj){
+  if(obj instanceof Demo){
+    obj.sayHello();
+  }
+  // obj && obj.sayHello && obj.sayHello();
+  // obj.sayHello();
+}
+
+let demo=new Demo();
+let test=new Test();
+
+abc(demo)
+abc(test)
+abc(100)
+
+console.log(demo instanceof Demo);
+console.log(test instanceof Test);
+console.log({} instanceof Demo);
+
