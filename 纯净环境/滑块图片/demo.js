b5b67319811d78a8efb4aad14a080a43a36d7760
YtMour <ResidualStar61@gmail.com>
2024-11-07 11:37:03 +0800

    js

---
+++ 纯净环境/滑块图片/demo.js
@@ -0,0 +1,128 @@
+
+
+let str = `
+<!--<div style="width: 260px; height: 116px; position: absolute; top: 10px; left: 10px;" id="Verification">-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-39px 0px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-195px -50px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-78px 0px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-104px -75px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-26px -50px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:0px -25px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-130px -75px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-182px 0px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-247px -75px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-91px -25px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-247px 0px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-130px 0px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-13px 0px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-52px -50px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-221px 0px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:0px 0px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-247px -25px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-65px 0px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-52px 0px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-52px -25px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-169px -25px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-156px -75px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-39px -25px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-156px -25px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-104px -50px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-117px -25px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-195px -75px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-143px -50px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-208px -25px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-143px -75px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-234px 0px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-234px -50px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-143px 0px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-65px -75px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-182px -75px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-221px -50px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-117px -50px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-26px -75px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-39px -50px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-234px -75px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-247px  -50px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-221px  -25px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-13px  -50px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-208px  -50px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-104px  0px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-104px  -25px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-195px  0px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-39px  -75px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-169px  -50px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-26px  -25px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-221px  -75px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-78px  -25px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-130px  -25px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-91px  0px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-13px  -25px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-143px  -25px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-117px  -75px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-26px  0px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:0px  -75px;"></div>-->
+// b'Wmprek1UbGxaamczTVE9PQ=='
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-195px  -25px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-65px  -50px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-169px  -75px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-91px  -50px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-91px  -75px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-208px  -75px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-65px  -25px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-234px  -25px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-169px  0px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-208px  0px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-130px  -50px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-182px  -50px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-156px  0px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-52px  -75px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:0px  -50px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-78px  -50px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-13px  -75px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-78px  -75px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-182px  -25px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-117px  0px;"></div>-->
+<!--    <div class="gt_cut_fullbg_slice" style="background-position:-156px  -50px;"></div>-->
+<!--</div>-->
+`;
+
+// let index = 0;
+// let retval = "";
+// let arr = str.matchAll(/"background-position:-?(\d+)px\s+-?(\d+)px;"/igs);
+// for (const regExpMatchArray of arr) {
+//     //console.log(regExpMatchArray[1] + ',' + regExpMatchArray[2]);
+//     retval += regExpMatchArray[1] + ',' + regExpMatchArray[2] + ","
+//     index++;
+// }
+// console.log("index: ", index);
+// console.log("retval: ", retval);
+
+
+let addrStr = "MzksMCwxOTUsNTAsNzgsMCwxMDQsNzUsMjYsNTAsMCwyNSwxMzAsNzUsMTgyLDAsMjQ3LDc1LDkxLDI1LDI0NywwLDEzMCwwLDEzLDAsNTIsNTAsMjIxLDAsMCwwLDI0NywyNSw2NSwwLDUyLDAsNTIsMjUsMTY5LDI1LDE1Niw3NSwzOSwyNSwxNTYsMjUsMTA0LDUwLDExNywyNSwxOTUsNzUsMTQzLDUwLDIwOCwyNSwxNDMsNzUsMjM0LDAsMjM0LDUwLDE0MywwLDY1LDc1LDE4Miw3NSwyMjEsNTAsMTE3LDUwLDI2LDc1LDM5LDUwLDIzNCw3NSwyNDcsNTAsMjIxLDI1LDEzLDUwLDIwOCw1MCwxMDQsMCwxMDQsMjUsMTk1LDAsMzksNzUsMTY5LDUwLDI2LDI1LDIyMSw3NSw3OCwyNSwxMzAsMjUsOTEsMCwxMywyNSwxNDMsMjUsMTE3LDc1LDI2LDAsMCw3NSwxOTUsMjUsNjUsNTAsMTY5LDc1LDkxLDUwLDkxLDc1LDIwOCw3NSw2NSwyNSwyMzQsMjUsMTY5LDAsMjA4LDAsMTMwLDUwLDE4Miw1MCwxNTYsMCw1Miw3NSwwLDUwLDc4LDUwLDEzLDc1LDc4LDc1LDE4MiwyNSwxMTcsMCwxNTYsNTA=";
+let crypto = require('./CryptoJS.js');
+let plainText = crypto.enc.Base64.parse(addrStr).toString(crypto.enc.Utf8);
+
+let addr = plainText.split(",");
+console.log(addr);
+
+let index = 0;
+let y = 0;
+let x = 0;
+for (let i = 0; i < addr.length; i += 2) {
+    //console.log(addr[i], addr[i + 1]);
+    index++;
+    if (x > 260) {
+        x = 0;
+    }
+    console.log(x);
+    x += 13;
+
+    if (index % 20 === 0) {
+        y += 25;
+        //console.log(y);
+    }
+
+}
+
+
+
